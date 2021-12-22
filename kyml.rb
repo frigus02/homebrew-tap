9f7f69e78063f64c7423d2fd54846e9ab14d89f3
Jan Kuehle <jkuehle90@gmail.com>
2021-12-22 18:53:56 +0100

    Revert "Use class instance instead of class variable"
    This reverts commit dc158e5cf10d97741f3952f5a81aa7a14b263d04.
    
    Using an instance variable seems to break bin.install. See
    https://github.com/frigus02/kyml/issues/30.
    
    Since I can't remember why I changed it to an instance variable in the
    first place, this reverts the commit. Fingers crossed this doesn't
    result in different issues.

--- kyml.rb
+++ kyml.rb
@@ -5,18 +5,18 @@ class Kyml < Formula
   version "20210610"
 
   if OS.mac? && Hardware::CPU.arm?
-    @bin_name = "kyml_#{version}_darwin_arm64"
+    @@bin_name = "kyml_#{version}_darwin_arm64"
     sha256 "e823288d23c95eab0454d60f02d1d10c59f016790c26c2723a9f313b341f4514"
   elsif OS.mac?
-    @bin_name = "kyml_#{version}_darwin_amd64"
+    @@bin_name = "kyml_#{version}_darwin_amd64"
     sha256 "d555393a079c2ba139d704dd16a3aa6fc7137ba25648d50663fbd2365576f3f9"
   elsif OS.linux?
-    @bin_name = "kyml_#{version}_linux_amd64"
+    @@bin_name = "kyml_#{version}_linux_amd64"
     sha256 "58be09ad7d971412fdb96ffcb7fd6abcfd498f95d8c6cde50e812562d835c918"
   end
-  url "https://github.com/frigus02/kyml/releases/download/v#{version}/#{@bin_name}"
+  url "https://github.com/frigus02/kyml/releases/download/v#{version}/#{@@bin_name}"
 
   def install
-    bin.install @bin_name => "kyml"
+    bin.install @@bin_name => "kyml"
   end
 end
